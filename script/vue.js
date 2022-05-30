0c53e1be6bd3f644b4493ac3aa11af12ca7b9d11
khj <zla1275@gamil.com>
2022-05-31 00:21:13 +0900

    서브메뉴 항목을 클릭했을 때 이벤트 핸들러 추가

--- script/vue.js
+++ script/vue.js
@@ -40,8 +40,37 @@ let vm = new Vue({
     onClickMainTitle: function () {
       location.reload();
     },
-    onClickSubItem: function () {
-      alert("클릭");
+    onClickSubItem: function (item) {
+      this.requestMenuUrl(item);
+    },
+    requestMenuUrl: function (item) {
+      let content = item.itemContent;
+
+      if (content.reqUrl === "reports") {
+        if (content.method === "GET") {
+          let startDate = this.formatDate(new Date());
+
+          let nowDate = new Date();
+          nowDate.setDate(nowDate.getDate() + 1);
+          let endDate = this.formatDate(nowDate);
+
+          axios
+            .get("http://localhost:8080/" + content.reqUrl + "?startDate=" + startDate + "&endDate=" + endDate)
+            .then(function (response) {
+              console.log(response.data);
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+        }
+      }
+    },
+    formatDate: function (date) {
+      let year = date.getFullYear();
+      let month = new String(date.getMonth() + 1).toString().padStart(2, "0");
+      let day = new String(date.getDate()).padStart(2, "0");
+
+      return year + "-" + month + "-" + day;
     },
   },
 });
